d6b0109fba3e7833edbddb17a0309cb1dd6087ae
GalaX <106726618+GalaX-OP@users.noreply.github.com>
2024-03-24 09:24:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,13 +29,13 @@ custom_messages = {
     """,
     ],
     "start": [
-        """**⭐  ➤➤「 To Start The Server  」**
+        """**⭐  ➤➤「 To Start The Server  」[StarLand]**
 
 **➥Step 1 GO TO **https://client.falixnodes.net/startserver 
 **➥Step 2 WRITE DOWN This IP **event.falixsrv.me
 **➥Step 3 VERIFY THE CAPTCHA**
 **➥Step 4 CLICK ON "START SERVER**
-          **<:fn:1206571088109371452>  ➤➤「 To Start The Server  」**
+          **<:fn:1206571088109371452>  ➤➤「 To Start The Server  」[FriendsNetworks]**
 
 **➥Step 1 GO TO **https://client.falixnodes.net/startserver 
 **➥Step 2 WRITE DOWN This IP **friendsnetworks.falixsrv.me
@@ -43,11 +43,17 @@ custom_messages = {
 **➥Step 4 CLICK ON "START SERVER**""",
     ],
     "time": [
-        """⭐ ➤➤「**Add Timer to Continue Playing!**」
+        """⭐ ➤➤「**Add Timer to Continue Playing! [StarLand]**」
 
 ➥ **Step 1** GO TO https://client.falixnodes.net/timer?id=1285958
 ➥ **Step 2** VERIFY THE CAPTCHA
-➥ **Step 3**  CLICK ON "**ADD TIME**""""",
+➥ **Step 3**  CLICK ON "**ADD TIME**
+           <:fn:1206571088109371452>➤➤「**Add Timer to Continue Playing! [FriendsNetworks**」
+
+➥ **Step 1** GO TO https://client.falixnodes.net/timer?id=1304060
+➥ **Step 2** VERIFY THE CAPTCHA
+➥ **Step 3**  CLICK ON "**ADD TIME**
+"""",
     ],
 }
 
